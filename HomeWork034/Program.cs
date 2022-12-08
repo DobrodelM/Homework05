ea53b4810e4e1de42df92756dd4ed86b8fe84c57
DobrodelM <118010165+DobrodelM@users.noreply.github.com>
2022-12-08 07:43:00 +0300

    Add files via upload

---
+++ HomeWork034/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+Задача 34.
+Задайте массив, заполненный случайными положительными трёхзначными числами.
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] generateRandomArray(int length, int start, int finish)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(start, finish + 1);
+    }
+    return resultArray;
+}
+
+int CountOfPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = generateRandomArray(4, 100, 999);
+printArray(array);
+int countPositive = CountOfPositiveNumbers(array);
+Console.WriteLine($"Количество четных чисел в массиве равно {countPositive}");
