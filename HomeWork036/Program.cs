2818e5d18c7c63b6fd5565d1e341c35bfbc2490e
DobrodelM <118010165+DobrodelM@users.noreply.github.com>
2022-12-08 07:43:21 +0300

    Add files via upload

---
+++ HomeWork036/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 36.
+Задайте одномерный массив, заполненный числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+
+int[] generateRandomArray(int length, int start, int finish)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(start, finish + 1);
+    }
+    return resultArray;
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = generateRandomArray(4, -99, 99);
+printArray(array);
+int summ = CountPositiveNumbers(array);
+Console.WriteLine($"Сумма элементов на нечетных позициях равна {summ}");
